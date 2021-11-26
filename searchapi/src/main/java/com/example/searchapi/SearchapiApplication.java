89fc6f80b2f534f48b005eb679d1bcf9b0ced4ce
darkgreen0924 <1429535947@qq.com>
2021-11-27 00:02:49 +0800

    create by dark

--- searchapi/src/main/java/com/example/searchapi/SearchapiApplication.java
+++ searchapi/src/main/java/com/example/searchapi/SearchapiApplication.java
@@ -8,12 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SearchapiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ioc = SpringApplication.run(SearchapiApplication.class, args);
-        String[] beanDefinitionNames = ioc.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
-        
+        SpringApplication.run(SearchapiApplication.class, args);
     }
 
 }
--- searchapi/src/test/java/com/example/searchapi/SearchapiApplicationTests.java
+++ searchapi/src/test/java/com/example/searchapi/SearchapiApplicationTests.java
@@ -1,22 +1,38 @@
 package com.example.searchapi;
 
 import com.example.searchapi.service.Search;
-import com.example.searchapi.service.SearchImpl;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class SearchapiApplicationTests {
     @Autowired
     @Qualifier("searchImpl")
-    Search search ;
+    private Search search ;
+    @Autowired
+    @Qualifier("restHighLevelClient")
+    private RestHighLevelClient client;
     @Test
     // 测试
     void contextLoads() {
         search.search("雷");
     }
-
+    @Test
+    public void test(){
+    }
 }
