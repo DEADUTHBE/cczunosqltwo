9855978a71aca733143179a9e3e09e881235cdf9
Eugene <caima12138@foxmail.com>
2021-11-27 16:43:06 +0800

    前后端分离

---
+++ server/searchapi/src/test/java/com/example/searchapi/SearchapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.searchapi;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchapiApplicationTests {
+
+    // 测试
+    void contextLoads() {
+
+    }
+    @Test
+    public void test(){
+    }
+}
