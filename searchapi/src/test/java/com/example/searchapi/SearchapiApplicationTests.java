2976fb5411401b64694941d5a89131ecc3fdd49a
bigdata0 <88190033+bigdata0@users.noreply.github.com>
2021-11-27 12:20:07 +0800

    Add files via upload
    更新了热搜模块

--- searchapi/src/test/java/com/example/searchapi/SearchapiApplicationTests.java
+++ searchapi/src/test/java/com/example/searchapi/SearchapiApplicationTests.java
@@ -1,36 +1,15 @@
 package com.example.searchapi;
 
-import com.example.searchapi.service.Search;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.client.indices.GetIndexRequest;
-import org.elasticsearch.client.indices.GetIndexResponse;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SearchapiApplicationTests {
-    @Autowired
-    @Qualifier("searchImpl")
-    private Search search ;
-    @Autowired
-    @Qualifier("restHighLevelClient")
-    private RestHighLevelClient client;
-    @Test
+
     // 测试
     void contextLoads() {
-        search.search("雷");
+
     }
     @Test
     public void test(){
