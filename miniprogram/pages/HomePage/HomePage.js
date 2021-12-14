b39cb5cecdb8864789e044e911bb83033878240b
Eugene <caima12138@foxmail.com>
2021-12-14 20:43:11 +0800

    主要功能全部完成

--- miniprogram/pages/HomePage/HomePage.js
+++ miniprogram/pages/HomePage/HomePage.js
@@ -1,22 +1,100 @@
 // pages/HomePage.js
+const db = wx.cloud.database({
+    env: 'woodpecker-demo-5gd5u8sl03d34a50'
+})
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        usersInfo: {},
+        word: "",
+        zhiZheUsersInfo: {}
     },
-    getData: function (params) {
-        wx.request({
-            url: 'http://42.192.38.199/hot?keyword=计算机&page=1',
+    getInput: function (e) {
+        let word = e.detail.value;
+        this.setData({
+            word: word
+        })
+    },
+    doSearchFromIcon: function () {
+        wx.navigateTo({
+            url: "DetailPage/DetailPage?word=" + this.data.word
+        })
+    },
+    doSearch: function (event) {
+        let word = event.detail.value;
+        wx.navigateTo({
+            url: "DetailPage/DetailPage?word=" + word
+        })
+    },
+    getTime: function () {
+        let time = new Date().getTime();
+        let date = new Date(time + 8 * 3600 * 1000); // 增加8小时
+        console.log(date);
+        return date.toJSON().substr(0, 19).replace('T', ' ');
+    },
+    getUserProfile(e) {
+        let that = this;
+        wx.getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+                that.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                });
+                db.collection("ZhiZheUsersInfo").add({
+                    data: {
+                        time: that.getTime(),
+                        gender: that.data.userInfo.gender ? "女" : "男",
+                        language: that.data.userInfo.language,
+                        nickName: that.data.userInfo.nickName,
+                        province: that.data.userInfo.province,
+                        avatarUrl: that.data.userInfo.avatarUrl,
+                        city: that.data.userInfo.city,
+                        country: that.data.userInfo.country
+                    },
+                    success: function (res) {
+                        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+                        console.log(res)
+                    }
+                })
+            }
         })
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
 
+
+        let that = this;
+        wx.getUserInfo({
+            success: function (res) {
+                var userInfo = res.userInfo
+                var nickName = userInfo.nickName
+                var avatarUrl = userInfo.avatarUrl
+                var gender = userInfo.gender //性别 0：未知、1：男、2：女
+                var province = userInfo.province
+                var city = userInfo.city
+                var country = userInfo.country
+                that.setData({
+                    userInfo: res.userInfo,
+                    nickName: userInfo.nickName,
+                    avatarUrl: userInfo.avatarUrl,
+                    gender: userInfo.gender, //性别 0：未知、1：男、2：女
+                    province: userInfo.province,
+                    city: userInfo.city,
+                    country: userInfo.country,
+                })
+            }
+        })
     },
 
     /**
